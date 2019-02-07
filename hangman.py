17f316eae5cbe0d12589c839f32b9cbf25808e49
Jia Cheng Ong <20966172+JiaChengOng96@users.noreply.github.com>
2019-02-06 23:45:40 -0500

    Simple GUI implementation

--- hangman.py
+++ hangman.py
@@ -1,44 +1,236 @@
-from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QSpacerItem
-from PyQt5.QtWidgets import QLabel, QPushButton, QLineEdit, QTextEdit
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QSpacerItem, QHBoxLayout
+from PyQt5.QtWidgets import QLabel, QPushButton, QLineEdit, QTextEdit, QRadioButton
+from PyQt5.QtCore import Qt, QTimer
+from PyQt5.QtGui import QMovie
+from Networking import Networking
+
+def create_widget_container(widgets, vertical = True):
+    """ The method to accept argument of list of widget and add it into the layout of vertical(default to vertical) else horizontal if specify and return the widget containing the layout """
+
+    new_widget = QWidget()
+
+    if not vertical:
+        new_layout = QHBoxLayout()
+    else:
+        new_layout = QVBoxLayout()
+    
+    for widget in widgets:
+        new_layout.addWidget(widget)
+    
+    new_widget.setLayout(new_layout)
+
+    return new_widget
+
 
 class Hangman():
-    """Creating a cless which encapsulates out our application"""
+    """ Creating a cless which encapsulates out our application """
     def __init__(self):
-        """ The constructor for this class"""
+        """ The constructor for this class """
 
         # Create a GUI application
         app = QApplication([])
 
         # Create our root window
-        window = QWidget()
+        window = QMainWindow()
 
-        # create the main empty layout and set the layout to the window
-        main_layout = QVBoxLayout()
-        window.setLayout(main_layout)
-
-        # create a label for the choice
-        lbl_side = QLabel("Which Side do you want?")
-
-        # Create the button for user to choose which side they want,
-        # Giver, or Guesser
-        btn_giver = QPushButton("Giver")
-        btn_guesser = QPushButton("Guesser")
-
-        # Add the button on the layout
-        main_layout.addWidget(lbl_side)
-        main_layout.addWidget(btn_giver)
-        main_layout.addWidget(btn_guesser)
-
+        self.create_main_pane()
+        self.create_selection_pane()
+        self.create_game_pane()
+        self.create_status_pane()
+        
+        window.setCentralWidget(self.connection_pane)
         window.show()
 
         self.app = app
         self.window = window
-        self.main_layout = main_layout
-        self.btn_giver = btn_giver
-        self.btn_guesser = btn_guesser
 
+        self.accepting = False
+        self.receiving = False
+        self.connection = None
+
+        self.timer = QTimer()
+        self.timer.start(100)
+        self.timer.timeout.connect(self.tick)
+        self.player = 0
+    
     def run(self):
         """ Method that start the application main loop """
         self.app.exec_()
 
         print("Application was closed")
+
+    def create_status_pane(self):
+
+        # Create the read only status box
+        status_box = QTextEdit()
+        status_box.setPlainText('')
+        status_box.setReadOnly(True)
+
+        status_pane = create_widget_container([status_box])
+
+        self.status_box = status_box
+        self.status_pane = status_pane
+
+    def create_game_pane(self):
+
+        game_box = QTextEdit()
+        game_box.setPlainText('')
+        game_box.setReadOnly(True)
+
+        lbl_character = QLabel('Enter the character')
+        inp_character = QLineEdit()
+        inp_character.returnPressed.connect(self.send_character)
+        
+        game_pane = create_widget_container([game_box, lbl_character,inp_character])
+
+        self.game_box = game_box
+        self.game_pane = game_pane
+    
+    def create_main_pane(self):
+        # Create a pane with the choice to connect or listen to others
+
+        # provide choice to connect or wait for connection
+        rad_connect = QRadioButton("Connect")
+        rad_listen = QRadioButton("Wait for Connection")
+
+        # initialise the connect radio button as checked
+        rad_connect.setChecked(True)
+
+        ## The connect pane
+
+        # Create the ip address txt box for ip input
+        lbl_connection_address = QLabel('IP address')
+        inp_connection_address = QLineEdit()
+        inp_connection_address.setText('localhost')
+
+        # the button to initiate the connection
+        btn_connect = QPushButton('Connect')
+        btn_connect.clicked.connect(self.btn_connect_clicked)
+
+        # asseble everything of connection pane into a container layout
+        connect_pane = create_widget_container([lbl_connection_address, inp_connection_address, btn_connect])
+
+        ## The listening pane
+
+        # Show the ip address of current machine
+        lbl_ip_address = QLabel(Networking.get_ip())
+
+        btn_listening = QPushButton("Wait for connection")
+        btn_listening.clicked.connect(self.btn_listen_clicked)
+
+        # assemble all listening widget into a container
+        listen_pane = create_widget_container([lbl_ip_address, btn_listening])
+
+        connection_pane = create_widget_container([rad_connect, rad_listen, connect_pane, listen_pane])
+
+        # set up a method to show or hide based on user choice
+        def show_connect_pane():
+            listen_pane.hide()
+            connection_pane.adjustSize()
+            connect_pane.show()
+
+        def show_listen_pane():
+            connect_pane.hide()
+            connection_pane.adjustSize()
+            listen_pane.show()
+
+        rad_connect.clicked.connect(show_connect_pane)
+        rad_listen.clicked.connect(show_listen_pane)
+
+        show_connect_pane()
+
+        self.connection_pane = connection_pane
+        self.inp_connection_address = inp_connection_address
+
+
+    def create_selection_pane(self):
+
+        # create a label for the choice
+        lbl_choose_side = QLabel("Which Side do you want?")
+
+        # Create the button for user to choose which side they want,
+        # Giver, or Guesser
+        btn_giver = QPushButton("Giver")
+        btn_giver.clicked.connect(self.btn_giver_clicked)
+        btn_guesser = QPushButton("Guesser")
+        btn_guesser.clicked.connect(self.btn_guesser_clicked)
+
+        # create the horizontal pane for the button
+        btn_group = create_widget_container([btn_giver, btn_guesser], False)
+
+        # create the pane vertically to store the label and button group
+        selection_pane = create_widget_container([lbl_choose_side, btn_group])
+
+        # create the label and button for the giver to give word
+        inp_word_enter = QLineEdit()
+        btn_word_enter = QPushButton("Send")
+        btn_word_enter.clicked.connect(self.send_word)
+
+        # giver pane
+        giver_pane = create_widget_container([inp_word_enter, btn_word_enter])
+
+        # create the waiting logo for the waiting side
+        label = QLabel("Loading....")
+        movie = QMovie("loading.gif")
+        label.setMovie(movie)
+        movie.start()
+
+        guesser_pane = create_widget_container([label])
+
+        self.inp_word_enter = inp_word_enter
+        self.giver_pane = giver_pane
+        self.guesser_pane = guesser_pane
+        self.selection_pane = selection_pane
+        
+
+    def btn_connect_clicked(self):
+        # when connect button is clicked, show the chat pane
+        self.window.setCentralWidget(self.giver_pane)
+
+        self.connection = Networking.Connection(self.inp_connection_address.text(), 5000)
+        print("Connecting into port 5000")
+    
+    def btn_listen_clicked(self):
+        # When listen button is clicked, show the selection pane
+        self.window.setCentralWidget(self.guesser_pane)
+        self.listener = Networking.Listener(5000)
+        print("Listening on port 5000")
+
+        self.accepting = True
+    
+    def btn_giver_clicked(self):
+        self.window.setCentralWidget(self.giver_pane)
+
+    def btn_guesser_clicked(self):
+        self.window.setCentralWidget(self.guesser_pane)
+
+    def send_character(self):
+        pass
+
+    def send_word(self):
+        self.word = self.inp_word_enter.text()
+
+        self.window.setCentralWidget(self.status_pane)
+        print("_ " * len(self.word))
+        self.status_box.append("_ " * len(self.word))
+
+    def tick(self):
+
+        if self.accepting:
+            self.connection = self.listener.try_get_connection()
+            if self.connection is not None:
+                self.accepting = False
+                self.receiving = True
+                print("Connected!!!")
+        
+        elif self.receiving:
+            they_sent = self.connection.try_receive()
+            if they_sent is not None:
+                display = 'Them: ' + str(they_sent, 'utf-8')
+                self.game_box.append(display)
+
+        elif self.connection is not None:
+            self.connection.try_connect()
+            if self.connection.connected:
+                self.receiving = True
+                print("Connected!!")
